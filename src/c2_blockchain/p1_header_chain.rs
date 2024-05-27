ff39bb95f8bb33be77857fdf6ebda537822f452a
Francisco Silva <franjs.francisco@gmail.com>
2024-05-27 16:58:15 +0200

    implementing p1_header_chain

--- src/c2_blockchain/p1_header_chain.rs
+++ src/c2_blockchain/p1_header_chain.rs
@@ -25,12 +25,24 @@ pub struct Header {
 impl Header {
     /// Returns a new valid genesis header.
     fn genesis() -> Self {
-        todo!("Exercise 1")
+        Header {
+            parent: 0,
+            height: 0,
+            extrinsics_root: (),
+            state_root: (),
+            consensus_digest: (),
+        }
     }
 
     /// Create and return a valid child header.
     fn child(&self) -> Self {
-        todo!("Exercise 2")
+        Header {
+            parent: hash(&self),
+            height: self.height + 1,
+            extrinsics_root: (),
+            state_root: (),
+            consensus_digest: (),
+        }
     }
 
     /// Verify that all the given headers form a valid chain from this header to the tip.
@@ -38,7 +50,11 @@ impl Header {
     /// This method may assume that the block on which it is called is valid, but it
     /// must verify all of the blocks in the slice;
     fn verify_sub_chain(&self, chain: &[Header]) -> bool {
-        todo!("Exercise 3")
+        match chain.len() {
+            0 => self.height == 0,
+            1 => chain[0].height == self.height + 1 && chain[0].parent == hash(&self),
+            _ => chain.iter().next().unwrap().verify_sub_chain(&chain[1..]),
+        }
     }
 }
 
@@ -46,14 +62,23 @@ impl Header {
 
 /// Build and return a valid chain with exactly five blocks including the genesis block.
 fn build_valid_chain_length_5() -> Vec<Header> {
-    todo!("Exercise 4")
+    let mut chain = vec![Header::genesis()];
+    for i in 0..5 {
+        let child = chain[i].child();
+        chain.push(child);
+    }
+    chain
 }
 
 /// Build and return a chain with at least three headers.
 /// The chain should start with a proper genesis header,
 /// but the entire chain should NOT be valid.
 fn build_an_invalid_chain() -> Vec<Header> {
-    todo!("Exercise 5")
+    let genesis = Header::genesis();
+    let block1 = genesis.child();
+    let block2 = block1.child();
+
+    vec![genesis, block2, block1]
 }
 
 // To run these tests: `cargo test bc_1
